cc384c301fc63e67a68832b1865d3f37bafb1a07
gorillainvestor <JimYin88@gmail.com>
2015-06-05 08:17:44 -0400

    Update Reverse a string.py

--- Reverse a string.py
+++ Reverse a string.py
@@ -10,6 +10,6 @@ def reverse_string2(a_string):
     if len(a_string) == 0:
         return
     else:
-        return reverse_string(a_string[1:]) + a_string[0]
+        return reverse_string2(a_string[1:]) + a_string[0]
     
 reverse_string2('abcdef')
