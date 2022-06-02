1112fe67fa2aa0f43347075459a0c91e47792aaf
Jim Yin <jimyin88@gmail.com>
2022-06-02 08:58:03 -0400

    Improve version

---
+++ common_elements_array.py
@@ -0,0 +1,10 @@
+'''
+Created on Jun 2, 2022
+
+@author: Jim Yin
+'''
+
+def common_elements(arr_a, arr_b):
+    return set(arr_a).intersection(set(arr_b))
+    
+print(common_elements([1, 2, 3, 4, 5, 6, 7], [3, 5, 7, 9, 11, 13]))
