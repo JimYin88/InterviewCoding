b860839060e0243ff394171da4057496bb76dfb0
Jim Yin <jimyin88@gmail.com>
2022-06-03 16:21:41 -0400

    Improved version

--- first_nonrepeating_character.py
+++ first_nonrepeating_character.py
@@ -1,10 +1,14 @@
+'''
+Created on Jun 3, 2022
+
+@author: JimYi
+'''
 import collections
 
 def non_repeated_character(string):
     result = collections.Counter(string)
     for letter in string:
         if result[letter] == 1:
-            print letter
-            break
+            return letter
             
-non_repeated_character('abababcddefc')
+print(non_repeated_character('abababcddefc'))
